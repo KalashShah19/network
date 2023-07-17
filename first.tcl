c9f3377576ec59d3596be9418d934272e96345c3
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2023-07-17 13:04:08 +0530

    first

---
+++ first.tcl
@@ -0,0 +1,38 @@
+#create a new simulator object
+
+set ns [new Simulator]
+
+#open the nam trace file
+
+set nf [open out.nam w]
+
+$ns namtrace-all $nf
+
+#define a 'finish' procedure
+
+proc finish {} {
+
+	global ns nf
+
+	$ns flush-trace
+
+	#close the trace file
+
+	close $nf
+
+	#execute nam on the trace file
+
+	exec nam out.nam &
+
+	exit 0
+}
+
+#create two nodes
+
+set n0 [$ns node]
+
+set n1 [$ns node]
+
+#create a duplex link between the nodes
+
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
