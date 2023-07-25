b5c6d40c1f0012acfa99b2cfac469336aa0bc191
KalashShah19 <92210070+KalashShah19@users.noreply.github.com>
2023-07-25 13:03:29 +0530

    topologies

--- bus.tcl
+++ bus.tcl
@@ -1,17 +1,14 @@
 # CREATING BUS TOPOLOGY
 
 #create a new simulator object
-
 set ns [new Simulator]
 
 #open the nam trace file
-
 set nf [open bus.nam w]
 
 $ns namtrace-all $nf
 
 #define a 'finish' procedure
-
 proc finish {} {
 
 	global ns nf
@@ -31,7 +28,6 @@ proc finish {} {
 }
 
 #create two nodes
-
 set n0 [$ns node]
 
 set n1 [$ns node]
@@ -41,7 +37,6 @@ set n2 [$ns node]
 set n3 [$ns node]
 
 #create a duplex link between the nodes
-
 $ns duplex-link $n0 $n1  1Mb 10ms DropTail
 
 $ns duplex-link $n1 $n2  1Mb 10ms DropTail
@@ -50,7 +45,6 @@ $ns duplex-link $n2 $n3  1Mb 10ms DropTail
 
 
 #create a udp agent and attach it to node n0
-
 set udp0 [new Agent/UDP]
 
 $ns attach-agent $n0 $udp0
@@ -61,7 +55,6 @@ $ns attach-agent $n1 $udp1
 
 
 #Create a CBR traffic source and attach it to udp0
-
 set cbr0 [new Application/Traffic/CBR]
 
 $cbr0 set packetSize_ 500
@@ -80,13 +73,11 @@ $cbr1 attach-agent $udp1
 
 
 #create a Null agent(a traffic sink) and attach it to node n1
-
 set null0 [new Agent/Null]
 
 $ns attach-agent $n1 $null0
 
 #Connect the traffic source to the sink
-
 $ns connect $udp0 $null0
 
 set null1 [new Agent/Null]
@@ -94,29 +85,20 @@ set null1 [new Agent/Null]
 $ns attach-agent $n2 $null1
 
 #Connect the traffic source to the sink
-
 $ns connect $udp1 $null1
 
 
 #Schedule events for CBR traffic
-
 $ns at 0.5 "$cbr0 start"
 
 $ns at 4.5 "$cbr0 stop"
-
-#call the finish procedure after 5 secs of simulated time
-
-$ns at 5.0 "finish"
-
 
 $ns at 0.5 "$cbr1 start"
 
 $ns at 4.5 "$cbr1 stop"
 
 #call the finish procedure after 5 secs of simulated time
-
 $ns at 5.0 "finish"
 
 #run the simulation
-
 $ns run
--- mesh.tcl
+++ mesh.tcl
@@ -1,15 +1,12 @@
 #create a new simulator object
-
 set ns [new Simulator]
 
 #open the nam trace file
-
 set nf [open mesh.nam w]
 
 $ns namtrace-all $nf
 
 #define a 'finish' procedure
-
 proc finish {} {
 
 	global ns nf
@@ -29,7 +26,6 @@ proc finish {} {
 }
 
 #create two nodes
-
 set n0 [$ns node]
 
 set n1 [$ns node]
@@ -38,7 +34,6 @@ set n2 [$ns node]
 
 
 #create a duplex link between the nodes
-
 $ns duplex-link $n0 $n1  1Mb 10ms DropTail
 
 $ns duplex-link $n1 $n2  1Mb 10ms DropTail
@@ -47,13 +42,11 @@ $ns duplex-link $n0 $n2  1Mb 10ms DropTail
 
 
 #create a udp agent and attach it to node n0
-
 set udp0 [new Agent/UDP]
 
 $ns attach-agent $n0 $udp0
 
 #Create a CBR traffic source and attach it to udp0
-
 set cbr0 [new Application/Traffic/CBR]
 
 $cbr0 set packetSize_ 500
@@ -62,26 +55,47 @@ $cbr0 set interval_ 0.005
 
 $cbr0 attach-agent $udp0
 
-#create a Null agent(a traffic sink) and attach it to node n1
+#create a udp agent and attach it to node n0
+set udp1 [new Agent/UDP]
 
+$ns attach-agent $n0 $udp1
+
+#Create a CBR traffic source and attach it to udp1
+set cbr1 [new Application/Traffic/CBR]
+
+$cbr1 set packetSize_ 500
+
+$cbr1 set interval_ 0.005
+
+$cbr1 attach-agent $udp1
+
+
+#create a Null agent(a traffic sink) and attach it to node n1
 set null0 [new Agent/Null]
 
 $ns attach-agent $n1 $null0
 
-#Connect the traffic source to the sink
+set null1 [new Agent/Null]
 
+$ns attach-agent $n2 $null1
+
+#Connect the traffic source to the sink
 $ns connect $udp0 $null0
 
-#Schedule events for CBR traffic
+$ns connect $udp1 $null1
 
+
+#Schedule events for CBR traffic
 $ns at 0.5 "$cbr0 start"
 
 $ns at 4.5 "$cbr0 stop"
 
-#call the finish procedure after 5 secs of simulated time
+$ns at 0.5 "$cbr1 start"
 
+$ns at 4.5 "$cbr1 stop"
+
+#call the finish procedure after 5 secs of simulated time
 $ns at 5.0 "finish"
 
 #run the simulation
-
 $ns run
